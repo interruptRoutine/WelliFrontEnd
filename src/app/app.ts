f841f181dc24ca6693b2b6f1a00d1f99ad3b466f
Riccardo <riccardo.zazza@icloud.com>
2025-10-21 18:24:27 +0200

    Creazione Progetto + Configurazione Proxy

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('WelliFrontEnd');
+
+  constructor(private http: HttpClient) {}
 }
